0fb9574dd7af4e502445ec0148300f15582ba4fd
aberke220 <aberke@deloitte.at>
2021-03-02 12:13:24 +0100

    2282 - Add gitlab actions for pushing Java containers to ECR
    - credit-application-service
    - accounts-service
    - transactions-service

--- data-access-services/docker-build.sh
+++ data-access-services/docker-build.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 set -e
 
+# $1 - Optional parameter to define which container to build
+#       DEFAULT: all
+#       'transactions-service'
+#       'accounts-service'
+
 # Build and verify all modules
 mvn clean install
 
 # Build containers for individual services
-mvn -f ./transactions-service/pom.xml \
- spring-boot:build-image \
- -Dspring-boot.build-image.imageName=core-banking/transactions-service \
- -DskipTests
-mvn -f ./accounts-service/pom.xml \
- spring-boot:build-image \
- -Dspring-boot.build-image.imageName=core-banking/accounts-service \
- -DskipTests
+
+if [ "$1" = "transactions-service" ] || [ -z "$1" ]; then
+    echo "Build core-banking/transactions-service"
+    mvn -f ./transactions-service/pom.xml \
+    spring-boot:build-image \
+    -Dspring-boot.build-image.imageName=core-banking/transactions-service \
+    -DskipTests
+ fi
+
+if [ "$1" = "accounts-service" ] || [ -z "$1" ]; then
+    echo "Build core-banking/accounts-service"
+    mvn -f ./accounts-service/pom.xml \
+    spring-boot:build-image \
+    -Dspring-boot.build-image.imageName=core-banking/accounts-service \
+    -DskipTests
+fi
