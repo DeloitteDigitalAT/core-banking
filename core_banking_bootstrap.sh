715c933a05e33bd038fead6b72cae0f0ef713805
amargapuramsathyanar <amargapuramsathyanar@deloitte.at>
2021-03-17 18:51:25 +0100

    updated bootstrap script to handles error cases

--- core_banking_bootstrap.sh
+++ core_banking_bootstrap.sh
@@ -1,44 +1,82 @@
 #!/bin/bash
 
-echo "Checking if docker daemon is running..."
-rep=$(curl -s --unix-socket /var/run/docker.sock http://ping > /dev/null)
+function print_usage() {
+  echo "[INFO]        Usage:"
+  echo "[INFO]              ./core_banking_bootstrap.sh [skipBuild]"
+  echo "[INFO]              skipBuild: optional flag to skip images build"
+  echo "[INFO]        Note: 1) must be run from the project root dir"
+  echo "[INFO]              2) docker must be running"
+}
+
+if [[ -n "$1" && "$1" != "skipBuild" ]]; then
+  echo "[ERROR] Unknown flag $1."
+  print_usage
+  exit 1
+fi
+
+echo "[INFO] Checking if docker daemon is running..."
+rep=$(curl -s --unix-socket /var/run/docker.sock http://ping >/dev/null)
 status=$?
 if [ "$status" == "7" ]; then
-    echo 'docker not running..'
-    exit 1
+  echo '[ERROR] docker is not running, please start docker.'
+  print_usage
+  exit 1
 fi
 
-echo "Stopping containers if already running..."
-cd ~/IdeaProjects/core-banking/
+echo "[INFO] Checking the current path..."
+path=$(pwd)
+if [[ ${path} != *"core-banking" ]]; then
+  echo "[ERROR] Current path ${path}, please run from the project root dir .../../core-banking."
+  print_usage
+  exit 1
+fi
+
+echo "[INFO] Stopping containers if already running..."
 docker-compose -f devops/docker/docker-compose.yml down
 
-echo "Building images..."
-cd ~/IdeaProjects/core-banking/
-echo "Building data-access-services images."
-cd data-access-services
-./docker-build.sh
-echo "Building credit-application-service image."
-cd ../domain-service/credit-application/
-./docker-build.sh
-echo "Building data-service image."
-cd ../../data-service
-./docker-build.sh
+if [ -z "$1" ]; then
+  echo "[INFO] Building images..."
+  echo "[INFO] Building data-access-services images."
+  cd data-access-services || (echo "[ERROR] Could not navigate to the data-access-services dir." && exit 1 )
+  ./docker-build.sh
+  echo "[INFO] Building credit-application-service image."
+  cd ../domain-service/credit-application/ || (echo "[ERROR] Could not navigate to the credit-application dir." && exit 1)
+  ./docker-build.sh
+  echo "[INFO] Building data-service image."
+  cd ../../data-service || (echo "[ERROR] Could not navigate to the data-service dir." && exit 1)
+  ./docker-build.sh
+  cd .. || (echo "[ERROR] Could not navigate to the project root dir." && exit 1)
+fi
 
-echo "Starting docker containers..."
-cd ~/IdeaProjects/core-banking/
+echo "[INFO] Starting docker containers..."
 docker-compose -f devops/docker/docker-compose.yml up -d
-NAMENODE_DOCKER_ID=$(docker ps -aqf "name=docker_namenode")
-HIVE_SERVER_DOCKER_ID=$(docker ps -aqf "name=docker_hive-server")
-if [ -z "$NAMENODE_DOCKER_ID" ]; then
-  echo "Could not find namenode docker container, please check if you have docker running."
+DATA_SERVICE_ID=$(docker ps -aqf "name=data-service*")
+ACCOUNTS_SERVICE_ID=$(docker ps -aqf "name=accounts-service*")
+TRANSACTIONS_SERVICE_ID=$(docker ps -aqf "name=transactions-service*")
+CREDIT_APPLICATION_SERVICE_ID=$(docker ps -aqf "name=credit-application-service*")
+if [ -z "$DATA_SERVICE_ID" ]; then
+  echo "[ERROR] Could not find data service docker container, please run the script again."
+  exit 1
 fi
 
-if [ -z "$HIVE_SERVER_DOCKER_ID" ]; then
-  echo "Could not find hive server docker container, please check if you have docker running."
+if [ -z "$ACCOUNTS_SERVICE_ID" ]; then
+  echo "[ERROR] Could not find accounts service docker container,  please run the script again."
+  exit 1
 fi
-echo "Docker compose started successfully"
 
-echo "Waiting for docker containers to warm up..."
+if [ -z "$TRANSACTIONS_SERVICE_ID" ]; then
+  echo "[ERROR] Could not find transactions service docker container,  please run the script again."
+  exit 1
+fi
+
+if [ -z "$CREDIT_APPLICATION_SERVICE_ID" ]; then
+  echo "[ERROR] Could not find credit application service docker container,  please run the script again."
+  exit 1
+fi
+
+echo "[INFO] Waiting for docker containers to warm up..."
 sleep 5s
 
-echo "Bootstrap of services and db's complete."
+echo "[INFO] Docker compose started successfully."
+echo "[INFO] Bootstrap of services and db's complete."
+docker ps
