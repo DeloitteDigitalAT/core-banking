bad61ac6003decb215334d22763184fa77cdf976
amargapuramsathyanar <amargapuramsathyanar@deloitte.at>
2021-04-09 19:06:38 +0200

    fixed sonar issues
    Please enter the commit message for your changes. Lines starting

--- domain-service/credit-application/src/main/java/at/deloittedigital/core_banking/credit/config/XRayInspector.java
+++ domain-service/credit-application/src/main/java/at/deloittedigital/core_banking/credit/config/XRayInspector.java
@@ -1,9 +1,6 @@
 package at.deloittedigital.core_banking.credit.config;
 
-import com.amazonaws.xray.entities.Subsegment;
 import com.amazonaws.xray.spring.aop.BaseAbstractXRayInterceptor;
-import java.util.Map;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
@@ -13,14 +10,9 @@ import org.springframework.stereotype.Component;
 public class XRayInspector extends BaseAbstractXRayInterceptor {
 
     @Override
-    protected Map<String, Map<String, Object>> generateMetadata(ProceedingJoinPoint proceedingJoinPoint,
-                                                                Subsegment subsegment) {
-        return super.generateMetadata(proceedingJoinPoint, subsegment);
-    }
-
-    @Override
     @Pointcut("@within(com.amazonaws.xray.spring.aop.XRayEnabled) && bean(*Controller)")
     public void xrayEnabledClasses() {
+        // For xray sdk to pick up enabled classes.
     }
 
 }
